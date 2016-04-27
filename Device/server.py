59cd0c5e0f44ff373e71d10c4a26f418098ed0f6
Damian Sepielak <damian.sepielak@gmail.com>
2016-04-27 22:02:28 +0200

    gui beginnings

--- Device/server.py
+++ Device/server.py
@@ -16,7 +16,7 @@ import ca_short as ca
 
 
 class CoreResource(resource.CoAPResource):
-	"""
+    """
 	Example Resource that provides list of links hosted by a server.
 	Normally it should be hosted at /.well-known/core
 	Resource should be initialized with "root" resource, which can be used
@@ -27,173 +27,177 @@ class CoreResource(resource.CoAPResource):
 	Notice that self.visible is not set - that means that resource won't
 	be listed in the link format it hosts.
 	"""
-	
-	def __init__(self, root):
-		resource.CoAPResource.__init__(self)
-		self.root = root
-	
-	def render_GET(self, request):
-		data = []
-		self.root.generateResourceList(data, "")
-		payload = ",".join(data)
-		print payload
-		response = coap.Message(code=coap.CONTENT, payload=payload)
-		response.opt.content_format = coap.media_types_rev['application/link-format']
-		return defer.succeed(response)
+
+    def __init__(self, root):
+        resource.CoAPResource.__init__(self)
+        self.root = root
+
+    def render_GET(self, request):
+        data = []
+        self.root.generateResourceList(data, "")
+        payload = ",".join(data)
+        print payload
+        response = coap.Message(code=coap.CONTENT, payload=payload)
+        response.opt.content_format = coap.media_types_rev['application/link-format']
+        return defer.succeed(response)
 
 
 class BedResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-		
-	def render_GET(self, request):
-		global bed_position
-		response_message = coap.Message(code=coap.CONTENT, payload='Bed position is: %d.' % bed_position)
-		return defer.succeed(response_message)
-	
-	def render_PUT(self, request):
-		global bed_position
-		bed_position = (int)(request.payload)
-		ca.set_dash(bed_position)
-		response_message = coap.Message(code=coap.CONTENT, payload="Bed position set to: %d." % bed_position)
-		return defer.succeed(response_message)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+
+    def render_GET(self, request):
+        global bed_position
+        response_message = coap.Message(code=coap.CONTENT, payload='Bed position is: %d.' % bed_position)
+        return defer.succeed(response_message)
+
+    def render_PUT(self, request):
+        global bed_position
+        bed_position = (int)(request.payload)
+        ca.set_dash(bed_position)
+        response_message = coap.Message(code=coap.CONTENT, payload="Bed position set to: %d." % bed_position)
+        return defer.succeed(response_message)
 
 
 class TemperatureResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-		self.observable = True
-		self.notify()
-		
-	def render_GET(self, request):
-		ca.query(ca.TEMP)
-		response_value = ca.get_res().value_of(ca.TEMP)
-		response_value = (response_value - 10) * 2
-		response_message = coap.Message(code=coap.CONTENT, payload="Temperature of patient is: %d." % response_value)
-		
-		temperature_file = open("temperatures.txt", "a")
-		temperature_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + (str)(response_value) + "\n")
-		temperature_file.close()
-		
-		return defer.succeed(response_message)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+        self.observable = True
+        self.notify()
 
-	def notify(self):
-		self.updatedState()
-		reactor.callLater(5, self.notify)
+    def render_GET(self, request):
+        ca.query(ca.TEMP)
+        response_value = ca.get_res().value_of(ca.TEMP)
+        response_value = (response_value - 10) * 2
+        response_message = coap.Message(code=coap.CONTENT, payload="Temperature of patient is: %d." % response_value)
+
+        temperature_file = open("temperatures.txt", "a")
+        temperature_file.write(
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + (str)(response_value) + "\n")
+        temperature_file.close()
+
+        return defer.succeed(response_message)
+
+    def notify(self):
+        self.updatedState()
+        reactor.callLater(5, self.notify)
 
 
 class TemperatureFileResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-	
-	def render_GET(self, request):
-		temperature_file = open("temperatures.txt", "r")
-		lines_content = temperature_file.readlines()
-		string_content = "".join(lines_content)
-		response_message = coap.Message(code=coap.CONTENT, payload=string_content)
-		return defer.succeed(response_message)
-	
-	def render_DELETE(self, request):
-		os.remove("temperatures.txt")
-		response_message = coap.Message(code=coap.CONTENT, payload="History of temperatures deleted successfully.")
-		return defer.succeed(response_message)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+
+    def render_GET(self, request):
+        temperature_file = open("temperatures.txt", "r")
+        lines_content = temperature_file.readlines()
+        string_content = "".join(lines_content)
+        response_message = coap.Message(code=coap.CONTENT, payload=string_content)
+        return defer.succeed(response_message)
+
+    def render_DELETE(self, request):
+        os.remove("temperatures.txt")
+        response_message = coap.Message(code=coap.CONTENT, payload="History of temperatures deleted successfully.")
+        return defer.succeed(response_message)
 
 
 class TemperatureProgramResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-	
-	def render_GET(self, request):
-		program_file = open("program_output.txt", "r")
-		lines_content = program_file.readlines()
-		string_content = "".join(lines_content)
-		response_message = coap.Message(code=coap.CONTENT, payload=string_content)
-		return defer.succeed(response_message)
-	
-	def thread_function(self, duration, arguments):
-		global alert
-		program_file = open("program_output.txt", "w")
-		for argument in arguments:
-			ca.query(ca.TEMP)
-			response_value = ca.get_res().value_of(ca.TEMP)
-			response_value = (response_value - 10) * 2
-			if (int)(argument) < response_value:
-				ca.set_led1(1)
-				alert = 1
-			program_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Expected: " + argument + "; is: " + (str)(response_value) + ".\n")
-			time.sleep(duration)
-		program_file.close()
-	
-	def render_PUT(self, request):
-		arguments = (request.payload).split(" ")
-		duration = (int)(arguments[0])
-		arguments.pop(0)
-		response_message = coap.Message(code=coap.CONTENT, payload="Program registered and started successfully.")
-		thread.start_new_thread(self.thread_function, (duration, arguments))
-		return defer.succeed(response_message)
-	
-	def render_DELETE(self, request):
-		os.remove("program_output.txt")
-		response_message = coap.Message(code=coap.CONTENT, payload="Program's output file deleted successfully.")
-		return defer.succeed(response_message)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+
+    def render_GET(self, request):
+        program_file = open("program_output.txt", "r")
+        lines_content = program_file.readlines()
+        string_content = "".join(lines_content)
+        response_message = coap.Message(code=coap.CONTENT, payload=string_content)
+        return defer.succeed(response_message)
+
+    @staticmethod
+    def thread_function(duration, arguments):
+        global alert
+        program_file = open("program_output.txt", "w")
+        for argument in arguments:
+            ca.query(ca.TEMP)
+            response_value = ca.get_res().value_of(ca.TEMP)
+            response_value = (response_value - 10) * 2
+            if (int)(argument) < response_value:
+                ca.set_led1(1)
+                alert = 1
+            program_file.write(
+                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Expected: " + argument + "; is: " + (str)(
+                            response_value) + ".\n")
+            time.sleep(duration)
+        program_file.close()
+
+    def render_PUT(self, request):
+        arguments = (request.payload).split(" ")
+        duration = (int)(arguments[0])
+        arguments.pop(0)
+        response_message = coap.Message(code=coap.CONTENT, payload="Program registered and started successfully.")
+        thread.start_new_thread(self.thread_function, (duration, arguments))
+        return defer.succeed(response_message)
+
+    def render_DELETE(self, request):
+        os.remove("program_output.txt")
+        response_message = coap.Message(code=coap.CONTENT, payload="Program's output file deleted successfully.")
+        return defer.succeed(response_message)
 
 
 class AlertResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-		self.observable = True
-		thread.start_new_thread(self.thread_function, ())
-		self.notify()
-	
-	def thread_function():
-		global alert
-		previous_state = 0
-		
-		ca.subscribe(ca.BUTTON1)
-		while True:
-			response_value = ca.get_res().value_of(ca.BUTTON1)
-			if response_value == 1:
-				if alert == 0:
-					alert = 1
-					ca.set_led1(1)
-				else:
-					alert = 0
-					ca.set_led1(0)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+        self.observable = True
+        thread.start_new_thread(self.thread_function, ())
+        self.notify()
 
+    @staticmethod
+    def thread_function():
+        global alert
+        previous_state = 0
 
-	def render_GET(self, request):
-		global alert
-		message_payload = "Everything is OK."
-		if alert == 1:
-			message_payload = "ALERT!"
-		response_message = coap.Message(code=coap.CONTENT, payload=message_payload)
-		return defer.succeed(response_message)
-	
-	def notify(self):
-		self.updatedState()
-		reactor.callLater(5, self.notify)
+        ca.subscribe(ca.BUTTON1)
+        while True:
+            response_value = ca.get_res().value_of(ca.BUTTON1)
+            if response_value == 1:
+                if alert == 0:
+                    alert = 1
+                    ca.set_led1(1)
+                else:
+                    alert = 0
+                    ca.set_led1(0)
+
+    def render_GET(self, request):
+        global alert
+        message_payload = "Everything is OK."
+        if alert == 1:
+            message_payload = "ALERT!"
+        response_message = coap.Message(code=coap.CONTENT, payload=message_payload)
+        return defer.succeed(response_message)
+
+    def notify(self):
+        self.updatedState()
+        reactor.callLater(5, self.notify)
 
 
 class PatientResource(resource.CoAPResource):
-	def __init__(self):
-		resource.CoAPResource.__init__(self)
-		self.visible = True
-	
-	def render_GET(self, request):
-		global patient_name
-		response_message = coap.Message(code=coap.CONTENT, payload=patient_name)
-		return defer.succeed(response_message)
-	
-	def render_PUT(self, request):
-		global patient_name
-		patient_name = request.payload
-		response_message = coap.Message(code=coap.CONTENT, payload="Patient name set to: " + patient_name + ".")
-		return defer.succeed(response_message)
+    def __init__(self):
+        resource.CoAPResource.__init__(self)
+        self.visible = True
+
+    def render_GET(self, request):
+        global patient_name
+        response_message = coap.Message(code=coap.CONTENT, payload=patient_name)
+        return defer.succeed(response_message)
+
+    def render_PUT(self, request):
+        global patient_name
+        patient_name = request.payload
+        response_message = coap.Message(code=coap.CONTENT, payload="Patient name set to: " + patient_name + ".")
+        return defer.succeed(response_message)
 
 
 # Global logic
@@ -233,5 +237,5 @@ hospital.putChild('TemperatureProgram', temperature_program)
 
 # Run server.
 endpoint = resource.Endpoint(root)
-reactor.listenUDP(coap.COAP_PORT, coap.Coap(endpoint)) #, interface="::")
+reactor.listenUDP(coap.COAP_PORT, coap.Coap(endpoint))  # , interface="::")
 reactor.run()
---
+++ hospital-client/src/main/java/pl/edu/agh/iot/hospital/client/Main.java
@@ -0,0 +1,37 @@
+package pl.edu.agh.iot.hospital.client;
+
+import com.sun.deploy.uitoolkit.impl.fx.ui.DialogTemplate;
+import com.sun.deploy.util.SystemUtils;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SmartHospital client");
+        initLayout(primaryStage);
+    }
+
+    private void initLayout(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/MainPane.fxml"));
+        try {
+            BorderPane rootLayout = loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
